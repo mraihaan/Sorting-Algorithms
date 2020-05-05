1ddc7e7fa9ed258ff4f2ae90a5aacb7790de569f
mraihaan <56098973+mraihaan@users.noreply.github.com>
2020-05-05 03:36:17 -0400

    Add files via upload

---
+++ sorting.js
@@ -0,0 +1,33 @@
+function selectionSort(arr){
+    for(var i=0; i<arr.length-1; i++){
+        var currentMin = i;
+        for (var j = i+1; j < arr.length; j++) {
+            if(arr[currentMin] > arr[j]){
+                currentMin = j;
+            }
+        }
+        var temp = arr[currentMin];
+        arr[currentMin] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+function insertionSort(arr){
+    for(var a=1; a<arr.length; a++){
+        var left=1;
+        var right=0;
+        if(arr[a-left] > arr[a]){
+            while(a-left>=0){
+                if(arr[a-left] > arr[a-right]){
+                    var temp = arr[a-left];
+                    arr[a-left] = arr[a-right];
+                    arr[a-right] = temp;
+                }
+                left++;
+                right++; 
+            }
+        }
+    }
+    return arr;
+}
